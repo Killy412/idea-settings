98458aab7defbcf4dca2408e9a959dc9c6df07dd
killy <killy@killy-PC>
2020-10-04 01:01:55 +0800

    IU-2020.2.2 <killy@killy-PC Create markdown.xml, chuanfu.xml, project.default.xml, equalsHashCodeTemplates.xml, Enum.java, Default copy.xml, Interface.java, applicationLibraries.xml, Default.xml, find.xml, one_dark_config.xml, spring-config.xml, KeyPromoterXSettings.xml, _@user_Darcula.icls, git.xml, Mybatis-Mapping.xml, Record.java, GrepConsole.xml, Default.xml, docker.xml, laf.xml, lightEdit.xml, Mybatis-Configuration.xml, editor.xml, debugger.xml, path.macros.xml, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_Default.icls, lombok-plugin.xml, terminal.xml, keymap.xml, ui.lnf.xml, filetypes.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, parameter.hints.xml, Class.java, jdk.table.xml, Jsp File.jsp, intentionSettings.xml, web-browsers.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * 
+ *
+ * @author  ChuanFu
+ * @date    ${YEAR}-${MONTH}-${DAY}
+ **/
+public enum ${NAME} {
+}
